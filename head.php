c411a7b15908e6d5b9a1282890d82310552e6e64
Kaustubh Vats <kaustubhvats08@gmail.com>
2022-02-15 22:24:01 +0530

    Initial commit

--- head.php
+++ head.php
@@ -1,9 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <meta name="description" content="A clone of a very famous OTT platform, Netflix. written in php. developed by Kaustubh Vats">
+    <meta name="title" content="Netflix India - Watch TV Shows Online, Watch Movies Online">
+    <meta name="author" content="Kaustubh Vats">
+    <meta name="country" content="IN">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords"
+        content="Kaustubh Vats, Netflix clone, php clone, php netflix clone">
+
+    <!-- Facebook meta tags -->
+    <meta property="og:type" content="Portfolio" />
+    <meta property="og:title" content="Netflix India - Watch TV Shows Online, Watch Movies Online" />
+    <meta property="og:description" content="A clone of a very famous OTT platform, Netflix. written in php. developed by Kaustubh Vats" />
+    <meta property="og:image" content="https://raw.githubusercontent.com/kaustubh-vats/netflix-clone/main/screenshot/ui.png" />
+    <meta property="og:url" content="https://netflix-clone-kaustubh.herokuapp.com/">
+
+    <!-- twitter meta tags -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://netflix-clone-kaustubh.herokuapp.com/">
+    <meta property="twitter:title"
+        content="Netflix India - Watch TV Shows Online, Watch Movies Online">
+    <meta property="twitter:description"
+        content="A clone of a very famous OTT platform, Netflix. written in php. developed by Kaustubh Vats">
+    <meta property="twitter:image"
+        content="https://raw.githubusercontent.com/kaustubh-vats/netflix-clone/main/screenshot/ui.png">
+
     <link type="text/css" rel="stylesheet" href="https://codex.nflxext.com/%5E3.0.0/truthBundle/webui/1.22.5-shakti-css-v9ac0b1cf/css/css/less%7Csignup%7Cnmhp%7Cnmhp.less/1/brxtD4BpszgawAC/none/true/none" data-uia="botLink">
     <link rel="shortcut icon" href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.ico"/>
     <link rel="stylesheet" href="css/header.css"/>
--- in-hi/head.php
+++ in-hi/head.php
@@ -1,9 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="hi">
 <head>
-    <meta charset="UTF-8">
+<meta name="description" content="एक बहुत ही प्रसिद्ध ओटीटी प्लेटफॉर्म नेटफ्लिक्स का क्लोन। php में लिखा है। कौस्तुभ वत्स द्वारा विकसित">
+    <meta name="title" content="Netflix भारत - टीवी शो और फ़िल्में ऑनलाइन देखें>
+    <meta name="author" content="कौस्तुभ वत्स">
+    <meta name="country" content="IN">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords"
+        content="कौस्तुभ वत्स, Netflix clone, php clone, php netflix clone">
+
+    <!-- Facebook meta tags -->
+    <meta property="og:type" content="Portfolio" />
+    <meta property="og:title" content="Netflix भारत - टीवी शो और फ़िल्में ऑनलाइन देखें />
+    <meta property="og:description" content="एक बहुत ही प्रसिद्ध ओटीटी प्लेटफॉर्म नेटफ्लिक्स का क्लोन। php में लिखा है। कौस्तुभ वत्स द्वारा विकसित" />
+    <meta property="og:image" content="https://raw.githubusercontent.com/kaustubh-vats/netflix-clone/main/screenshot/ui.png" />
+    <meta property="og:url" content="https://netflix-clone-kaustubh.herokuapp.com/">
+
+    <!-- twitter meta tags -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://netflix-clone-kaustubh.herokuapp.com/">
+    <meta property="twitter:title"
+        content="Netflix भारत - टीवी शो और फ़िल्में ऑनलाइन देखें>
+    <meta property="twitter:description"
+        content="एक बहुत ही प्रसिद्ध ओटीटी प्लेटफॉर्म नेटफ्लिक्स का क्लोन। php में लिखा है। कौस्तुभ वत्स द्वारा विकसित">
+    <meta property="twitter:image"
+        content="https://raw.githubusercontent.com/kaustubh-vats/netflix-clone/main/screenshot/ui.png">
+
     <link type="text/css" rel="stylesheet" href="https://codex.nflxext.com/%5E3.0.0/truthBundle/webui/1.22.5-shakti-css-v9ac0b1cf/css/css/less%7Csignup%7Cnmhp%7Cnmhp.less/1/brxtD4BpszgawAC/none/true/none" data-uia="botLink">
     <link rel="shortcut icon" href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.ico"/>
     <link rel="stylesheet" href="/css/header.css"/>
